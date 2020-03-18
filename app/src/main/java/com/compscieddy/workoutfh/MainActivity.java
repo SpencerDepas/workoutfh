ef43321ec9a6e4b486eac92deb0975129318bccd
Eddie Lee <eddieberklee@gmail.com>
2020-03-18 01:49:36 -0700

    Added logout button

--- app/src/main/java/com/compscieddy/workoutfh/MainActivity.java
+++ app/src/main/java/com/compscieddy/workoutfh/MainActivity.java
@@ -1,12 +1,47 @@
 package com.compscieddy.workoutfh;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends BaseActivity {
+
+  @BindView(R.id.logout_button) View mLogoutButton;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+    ButterKnife.bind(MainActivity.this);
+  }
+
+  @Override
+  protected void onResume() {
+    super.onResume();
+    attachListeners();
+  }
+
+  @Override
+  protected void onPause() {
+    super.onPause();
+    detachListeners();
+  }
+
+  private void attachListeners() {
+    mLogoutButton.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View view) {
+        FirebaseAuth.getInstance().signOut();
+        ActivityHelper.launchActivity(MainActivity.this, AuthenticationActivity.class);
+      }
+    });
+  }
+
+  private void detachListeners() {
+    mLogoutButton.setOnClickListener(null);
   }
 }
