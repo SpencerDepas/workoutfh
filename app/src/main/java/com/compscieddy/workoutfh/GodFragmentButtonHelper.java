dbfce4a4e6638c22f0f235928622bb011b3be228
Eddie Lee <eddieberklee@gmail.com>
2020-03-21 16:59:04 -0700

    adjustment of values

--- app/src/main/java/com/compscieddy/workoutfh/GodFragmentButtonHelper.java
+++ app/src/main/java/com/compscieddy/workoutfh/GodFragmentButtonHelper.java
@@ -7,7 +7,7 @@ public class GodFragmentButtonHelper {
   public static void setGodFragmentButtonSelectedState(boolean isSelected, View godFragmentButton) {
     final float SELECTED_ALPHA = 1.0f;
     final float UNSELECTED_ALPHA = 0.5f;
-    final float SELECTED_SCALE = 1.3f;
+    final float SELECTED_SCALE = 1.2f;
     final float UNSELECTED_SCALE = 1.0f;
 
     godFragmentButton.setAlpha(isSelected ? SELECTED_ALPHA : UNSELECTED_ALPHA);
